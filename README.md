3442c9f585af084b3180f01f0145994dd3746ddb
timoerb <timoer.buckinx@ixor.be>
2018-12-14 11:39:04 +0100

    Testing

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Darkspark
+
+This app is a game which only works with the OBSS services. 
+
+## Story 
--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './random.svg';
 import './App.css';
 
 class App extends Component {
