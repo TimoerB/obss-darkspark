3ff2f00622308374712942fc63bf1eb88635bc84
TimoerB <43813271+TimoerB@users.noreply.github.com>
2018-12-16 18:25:19 +0100

    Test backbone structure

--- src/App.js
+++ src/App.js
@@ -3,23 +3,23 @@ import logo from './random.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <a
-            className="App-link"
-            href="https://obss.be"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            OBSS
-          </a>
-        </header>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <a
+                        className="App-link"
+                        href="https://obss.be"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        OBSS
+                    </a>
+                </header>
+            </div>
+        );
+    }
 }
 
 export default App;
